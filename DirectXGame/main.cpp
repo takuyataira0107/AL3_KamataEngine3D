0841d2ca5901558a9c8804666f3a1456d6ad82fa
takuyataira0107 <146914986+takuyataira0107@users.noreply.github.com>
2024-04-18 11:58:08 +0900

    1-1初コミット

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2C_15_タイラ_タクヤ_AL3");
+	win->CreateGameWindow(L"LE2C_15_タイラ_タクヤ_AL3"); 
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
