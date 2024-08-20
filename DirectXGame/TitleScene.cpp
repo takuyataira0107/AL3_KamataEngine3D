69682ca40d116cd383adf61c8ea1546b60fa585f
takuyataira0107 <146914986+takuyataira0107@users.noreply.github.com>
2024-08-20 22:49:08 +0900

    test

--- DirectXGame/TitleScene.cpp
+++ DirectXGame/TitleScene.cpp
@@ -1,8 +1,22 @@
 #include "TitleScene.h"
 #include <Input.h>
+#include "TextureManager.h"
+
+TitleScene::TitleScene() {
+
+}
+
+TitleScene::~TitleScene() { 
+	delete sprite_; 
+}
 
 void TitleScene::Initialize() {
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
 
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
 }
 
 void TitleScene::Update() { 
@@ -12,5 +26,47 @@ void TitleScene::Update() {
 }
 
 void TitleScene::Draw() {
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	sprite_->Draw();
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
 }
--- DirectXGame/TitleScene.h
+++ DirectXGame/TitleScene.h
@@ -1,3 +1,8 @@
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include <Model.h>
+#include <Sprite.h>
 #pragma once
 
 /// <summary>
@@ -5,6 +10,16 @@
 /// </summary>
 class TitleScene {
 public:
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	TitleScene();
+
+	/// <summary>
+	/// デストクラタ
+	/// </summary>
+	~TitleScene();
+
 	/// <summary>
 	/// 初期化
 	/// </summary>
@@ -24,6 +39,13 @@ public:
 	bool IsFinished() const { return finished_; }
 
 private:
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
 	// 終了フラグ
 	bool finished_ = false;
+
+	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
 };
